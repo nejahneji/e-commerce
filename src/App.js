df6802bf7f8944ee684c1369ae6dd2b47c8a5705
unknown <you@example.com>
2022-01-12 18:57:26 +0100

    add withrouter to navigate into my menu items components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+
+const Hats =(props) => {
+  // console.log(props)
+  return (
+  <div>
+    <h1>hats</h1>
+  </div>
+)}
+
 
 
 function App() {
   return (
-    <div >
-      <HomePage/>
+    <Router>
+    <div>
+      <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/shop/hats' component={Hats}/>
+     </Switch>
     </div>
+    </Router>  
   );
 }
 
