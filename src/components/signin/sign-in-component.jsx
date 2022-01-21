6d7d9990c1da9f6020df99d28ff98995ea18b4e4
unknown <you@example.com>
2022-01-21 17:23:44 +0100

    sign in with email and password

--- src/components/signin/sign-in-component.jsx
+++ src/components/signin/sign-in-component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth , signInWithGoogle } from "../../firebase/firebase.utils";
 import CustomButton from "../custom-button/custom-button.component";
 import FormInput from "../form-input/form-input.component";
 import "./sign-in.styles.scss";
@@ -12,9 +12,17 @@ class SignIn extends React.Component {
       password: "",
     };
   }
-  handleSubmit = (e) => {
+  handleSubmit = async e => {
     e.preventDefault();
-    this.setState({ email: "", password: "" });
+    const {email, password} =  this.state ;
+    try {
+      await auth.signInWithEmailAndPassword(email , password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error)
+    }
+
+    
   };
   handleChange = (e) => {
     const { value, name } = e.target;
