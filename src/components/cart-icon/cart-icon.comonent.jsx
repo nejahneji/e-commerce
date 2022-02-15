28488a3cfa71495d2c0539a9276edb0da64eef6a
unknown <you@example.com>
2022-02-15 23:08:13 +0100

    using reslect library , replace cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.comonent.jsx
+++ src/components/cart-icon/cart-icon.comonent.jsx
@@ -2,14 +2,15 @@ import React from 'react' ;
 import { connect } from 'react-redux';
 import { ReactComponent as ShoppinIcon} from '../../assets/shopping-bag.svg'
 import { ToggleCartHidden } from '../../redux/cart/cart.action';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectore';
 
 
 import './cart-icon.styles.scss' ;
 
-const CartIcon =({ToggleCartHidden}) => (
+const CartIcon =({ToggleCartHidden , itemCount}) => (
 <div className='cart-icon' onClick={ToggleCartHidden}>
     <ShoppinIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemCount}</span>
 </div>
 ) 
 
@@ -17,4 +18,8 @@ const mapDispatchToProps = dispatch => ({
     ToggleCartHidden : () => dispatch(ToggleCartHidden())
 })
 
-export default connect(null,mapDispatchToProps)(CartIcon)
+const mapStateToPrps = (state) => ({
+   itemCount : selectCartItemsCount(state)
+})
+
+export default connect(mapStateToPrps,mapDispatchToProps)(CartIcon)
