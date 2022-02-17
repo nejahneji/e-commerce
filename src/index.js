e7868d32a25270fd8f719a6e876288ab03c351cd
unknown <you@example.com>
2022-02-18 00:22:56 +0100

    installing redux-persist to store our cart in localStorage

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { PersistGate } from 'redux-persist/integration/react'
+import {persistor} from './redux/store'
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import {store} from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
+      <PersistGate persistor={persistor}>
       <App />
+      </PersistGate>
     </React.StrictMode>
     </Provider> ,
   document.getElementById("root")
